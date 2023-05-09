91d687104fdfd6b386a7789aeae50dc31b9f598b
htaylor91 <htaylor91@gmail.com>
2023-05-09 17:13:11 +0200

    feat(footer): ✨ add Footer component

---
+++ Front/src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import '../styles/footer.css'
+
+function Footer(){
+    return (
+        <footer className="footer">
+            <p className="footer-text">Copyright {new Date().getFullYear()} Argent Bank</p>
+        </footer>
+    )
+}
+
+export default Footer
