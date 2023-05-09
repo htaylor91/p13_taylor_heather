2dffb190f9ea24c1c766b0024efb92e013712846
htaylor91 <htaylor91@gmail.com>
2023-05-09 18:44:16 +0200

    feat: ✨ add SignInPage, PrivateOutlet, and ProfilePage routes to the App component
    closes #2, closes #4

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -2,14 +2,25 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './routes/Layout';
 import HomePage from './routes/HomePage';
 import ErrorPage from './routes/ErrorPage';
+import SignInPage from './routes/SignInPage';
+import PrivateOutlet from './routes/PrivateOutlet';
+import ProfilePage from './features/user/ProfilePage';
 
 function App(){
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
+            {/* public routes */}
             <Route index element={<HomePage />} />
+            <Route path="/login" element={<SignInPage />} />
+
+            {/* protected routes */}
+            <Route path="/profile" element={<PrivateOutlet />}>
+              <Route index element={<ProfilePage />} />
+            </Route>
         </Route>
+
         <Route path="*" element={<ErrorPage/>} />
       </Routes>
     </BrowserRouter>
